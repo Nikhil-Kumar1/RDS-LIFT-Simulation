9418bf1114b249d4bc1ded9ce8ec686a4526f828
Nikhil-Kumar1 <knikhil408@gmail.com>
2023-08-04 22:10:04 +0530

    commit1

--- script.js
+++ script.js
@@ -15,18 +15,34 @@ for(i=floorsValue; i>0;i--){
   // let floors = document.createElement("hr");
   floors.setAttribute("class","flooring")
 
-  let buttonContainer = document.createElement("div")
-  buttonContainer.setAttribute("class","buttons")
+  let buttonContainer = document.createElement("div");
+  buttonContainer.setAttribute("class","buttons");
   let Upbuttons = document.createElement("button");
+  Upbuttons.setAttribute("class","Upbtn");
   Upbuttons.innerText="Up";
   let Downbuttons = document.createElement("button");
   Downbuttons.innerText = 'Down'
   
   let para = document.createElement("p");
-  buttonContainer.append(Upbuttons,Downbuttons);
-  floordiv.append(buttonContainer,para);
+  
+  let textPara = document.createTextNode(`Floor ${i}`)
+  let floorNum = para.appendChild(textPara);
+  buttonContainer.append(floorNum,Upbuttons,Downbuttons);
+  floordiv.append(buttonContainer,);
   render.appendChild(floordiv);
 }
+
+for(i=liftsValue; i>0;i--){
+  let lift = document.createElement("div");
+  lift.setAttribute("class","lift");
+  let Lgate = document.createElement("div");
+  Lgate.setAttribute("class","l-gate");
+  let Rgate = document.createElement("div");
+  Rgate.setAttribute("class","r-gate");
+  lift.append(Lgate,Rgate);
+  render.appendChild(lift);
+}
+
 
 // for (i =liftsValue; i>0;i-- ){
 //   let liftdiv = document.createElement(div);
